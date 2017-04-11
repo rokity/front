5f2a246bae55e7baccba43a66ea17708f54a0e37
riccardo <riccardo.amadio1@gmail.com>
2017-04-11 10:48:47 +0200

    Update name of this framework
    I call this framework 'front' because I begin this project after many weeks
    which I had problems to routing and rendering my front-end project .
    So after test of express,hapi and other framework I have the needing to create my custom front-end framework for routing and rendering.

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 var http=require("http");
-
+var fs=require("fs");
 http.createServer((req, res) => {
-   res.writeHead(200, {"Content-Type": "text/html"});
-	var fs=require("fs");
-var readStream=fs.createReadStream("index.html",{});
-var data="";
-readStream.on('data',(chunk) => { data += chunk; });
-readStream.on('end',() => {
-   res.end(data);});
-}).listen(8000);
+   res.writeHead(200, {"Content-Type": "text/html"});   
+   var readStream=fs.createReadStream("index.html",{});
+   var data="";
+   readStream.on('data',(chunk) => { data += chunk; });
+   readStream.on('end',() => { res.end(data); });
+    }).listen(8000);
