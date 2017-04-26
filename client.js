17dc079ae1fa1362e6469a084be19cd649c1153a
riccardo <riccardo.amadio1@gmail.com>
2017-04-26 15:45:35 +0200

    Routes and Route
    I added two class that give the possibility to create and add routes permantely .

---
+++ client.js
@@ -0,0 +1,12 @@
+'use strict'
+/*
+var Route = require('./lib/route')
+ var home = new Route("/home", "home.html")
+var about = new Route("/about", "about.html")
+
+var Routes = require('./lib/routes')
+var routes = new Routes()
+
+/* console.log(routes.getRoute("/"))
+console.log(routes.getRoute("/home"))
+console.log(routes.getRoute("/about")) */
