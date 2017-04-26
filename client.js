38e048157511faf2fa92e4550e04322d6e8b5b10
riccardo <riccardo.amadio1@gmail.com>
2017-04-26 16:23:48 +0200

    Routing works fine

--- client.js
+++ client.js
@@ -1,12 +1,21 @@
 'use strict'
-/*
-var Route = require('./lib/route')
+
+/* var Route = require('./lib/route')
  var home = new Route("/home", "home.html")
 var about = new Route("/about", "about.html")
 
 var Routes = require('./lib/routes')
 var routes = new Routes()
 
-/* console.log(routes.getRoute("/"))
-console.log(routes.getRoute("/home"))
-console.log(routes.getRoute("/about")) */
+ console.log(routes.getRoute("/"))
+console.log(routes.getRoute("/home")) */
+
+var server = require('./lib')
+
+var Route = server.route
+var home = new Route('/home', 'index.html')
+
+var routes = server.routes
+routes.add(home)
+
+server.start(routes, 3000)
