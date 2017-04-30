c3a30249c4d9bb06d70400990c7f8a9ec26b74dd
riccardo <riccardo.amadio1@gmail.com>
2017-04-30 22:01:51 +0200

    templating with handlebars

--- client.js
+++ client.js
@@ -3,15 +3,15 @@
 const server = require('./lib');
 
 const Route = server.route;
-const home = new Route('/home', 'index.html');
-var routes = server.routes;
+const home = new Route('/home', 'index.html', { title: 'STOCAZZO', body: 'AMBE' });
+const routes = server.routes;
 routes.add(home);
 
-var index = new Route('/',(req,res) =>{    res.end("STOCAZZOOO"); });
+const index = new Route('/', (req, res) => { res.end('STOCAZZOOO'); });
 routes.add(index);
 server.static('static', 'static');
 
-server.setViewPath("view");
-//console.log(server.viewPath);
+server.setViewPath('view');
+// console.log(server.viewPath);
 
 server.start(routes, 3000);
