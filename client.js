154baa745aad44564f5b25972e9ab925892a1df5
riccardo <riccardo.amadio1@gmail.com>
2017-05-05 17:47:00 +0200

    accedi

--- client.js
+++ client.js
@@ -7,12 +7,21 @@ server.setViewPath('view');
 const Route = server.route;
 const routes = server.routes;
 
-const obj = {};
-obj.element = '<my-app-home></my-app-home>';
-obj.elementUrl = 'static/components/home.html';
-const json = JSON.stringify(obj);
-const home = new Route('/home', 'home_template.html', json);
-routes.add(home);
+// Define Home Route
+const homeContext = {};
+homeContext.element = '<my-app-home></my-app-home>';
+homeContext.elementUrl = 'static/components/home.html';
+const jsonHomeContext = JSON.stringify(homeContext);
+const homeRoute = new Route('/home', 'home_template.html', jsonHomeContext);
+routes.add(homeRoute);
+
+//Define Accedi Route
+const accediContext = {};
+accediContext.element = '<my-app-accedi></my-app-accedi>';
+accediContext.elementUrl = 'static/components/accedi.html';
+const jsonAccediContext = JSON.stringify(accediContext);
+const accediRoute = new Route('/accedi', 'home_template.html', jsonAccediContext);
+routes.add(accediRoute);
 
 
 server.start(routes, 3000);
