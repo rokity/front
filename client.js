90fe602d7bc32f34b99aefb9d203d9f5304bae52
riccardo <riccardo.amadio1@gmail.com>
2017-05-07 19:47:54 +0200

    vulcanized added with problem with images
    Images must be loaded separated from static files like js or css because vulcanize doesn't compress it.

--- client.js
+++ client.js
@@ -10,18 +10,21 @@ const routes = server.routes;
 // Define Home Route
 const homeContext = {};
 homeContext.element = '<my-app-home></my-app-home>';
-homeContext.elementUrl = 'static/components/home.html';
+homeContext.elementUrl = 'static/dist/components/home.html';
 const jsonHomeContext = JSON.stringify(homeContext);
 const homeRoute = new Route('/home', 'home_template.html', jsonHomeContext);
 routes.add(homeRoute);
 
-//Define Accedi Route
+// Define Accedi Route
 const accediContext = {};
 accediContext.element = '<my-app-accedi></my-app-accedi>';
-accediContext.elementUrl = 'static/components/accedi.html';
+accediContext.elementUrl = 'static/dist/components/accedi.html';
 const jsonAccediContext = JSON.stringify(accediContext);
 const accediRoute = new Route('/accedi', 'home_template.html', jsonAccediContext);
 routes.add(accediRoute);
 
 
+server.vulcanize();
+
 server.start(routes, 3000);
+
